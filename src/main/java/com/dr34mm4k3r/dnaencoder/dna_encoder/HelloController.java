c5f66ff08b1b8dadec0ea6c98f8a6d216b0f64da
DR34M-M4K3R <v.michoux@gmail.com>
2021-11-27 18:56:39 +0100

    Initial commit

---
+++ src/main/java/com/dr34mm4k3r/dnaencoder/dna_encoder/HelloController.java
@@ -0,0 +1,80 @@
+package com.dr34mm4k3r.dnaencoder.dna_encoder;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.Objects;
+
+
+public class HelloController {
+
+    static JFrame frame;
+    /*    public static void choixFichier(String indicator){
+            frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setTitle("Titre");
+            FileDialog fd = new FileDialog(frame, "Choose a file", FileDialog.LOAD);
+
+            //fd.setFile("*.dna");
+            // for mac os (?) --> fd.setFilenameFilter((dir, name) -> name.endsWith(".dna"));
+            fd.setVisible(true);
+            frame.setVisible(true);
+            String filename = fd.getFile();
+            System.out.println(filename);
+           // if (filename == null){
+             //   return null;
+          //  }else{
+                //return(filename);
+
+        }*/
+    public static void choixFichier(String indicator){
+
+        if (Objects.equals(indicator, "encode")) {
+            FileChooser f = new FileChooser();
+            File selectedFile = f.showOpenDialog(null);
+            System.out.println(selectedFile.getAbsolutePath());
+        }
+
+
+    }
+
+
+
+    @FXML
+    private Button encodeButton;
+
+    @FXML
+    private Button decodeButton;
+
+    @FXML
+    private Button aboutButton;
+
+
+
+    @FXML
+    protected void encodeAction() {
+        System.out.println("encode");
+        choixFichier("encode");
+
+    }
+    @FXML
+    protected void decodeAction() {
+        System.out.println("decode");
+        choixFichier("decode");
+    }
+    @FXML
+    protected void aboutAction() {
+        System.out.println("about");
+    }
+
+
+
+}
