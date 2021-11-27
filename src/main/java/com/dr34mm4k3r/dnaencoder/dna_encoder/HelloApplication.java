c00a3a73310eb0a2f3bce5400ffc7f5594061a06
DR34M-M4K3R <v.michoux@gmail.com>
2021-11-27 18:59:26 +0100

    added licence headers

--- src/main/java/com/dr34mm4k3r/dnaencoder/dna_encoder/HelloApplication.java
+++ src/main/java/com/dr34mm4k3r/dnaencoder/dna_encoder/HelloApplication.java
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * original creator: DR34M-M4K3R
+ */
+
 package com.dr34mm4k3r.dnaencoder.dna_encoder;
 
 import javafx.application.Application;
